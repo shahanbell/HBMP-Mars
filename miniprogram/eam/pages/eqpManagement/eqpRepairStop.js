11a702847aba2a391b0a9387b33de47ffa7ea0f8
沈越_SHB_资讯室 <841873930@qq.com>
2021-03-03 16:23:47 +0800

    2021-03-03_设备管理小程序端v1.4.2_Update
    1.修复苹果手机日期格式化失败的问题

--- miniprogram/eam/pages/eqpManagement/eqpRepairStop.js
+++ miniprogram/eam/pages/eqpManagement/eqpRepairStop.js
@@ -1,5 +1,6 @@
 // miniprogram/pages/eqpManagement/startEqpRepair.js
 import Dialog from '../../../component/vant/dialog/dialog';
+var util = require("../../../utils/eamCommonUtils.js");
 const date = new Date()
 const years = []
 const months = []
@@ -1311,7 +1312,7 @@ upload: function(e) {
             newItem.pId = repairHisListInfo[i].pid;
             newItem.userNo = repairHisListInfo[i].userno;
             newItem.userName = repairHisListInfo[i].username;
-            newItem.creDate = _this.utcInit(repairHisListInfo[i].credate);
+            newItem.creDate = util.utcInit(repairHisListInfo[i].credate);
             newItem.contenct = repairHisListInfo[i].contenct;
             newItem.note = repairHisListInfo[i].note;
             _this.data.repairHisList.push(newItem);
@@ -1338,39 +1339,6 @@ upload: function(e) {
         });
       }
     });
-  },
-
-  utcInit: function(utc_datetime) {
-    // 转为正常的时间格式 年-月-日 时:分:秒
-    var T_pos = utc_datetime.indexOf('T');
-    var Z_pos = utc_datetime.indexOf('Z');
-    var year_month_day = utc_datetime.substr(0,T_pos);
-    var hour_minute_second = utc_datetime.substr(T_pos+1,Z_pos-T_pos-1);
-    var new_datetime = year_month_day+" "+hour_minute_second; // 2017-03-31 08:02:06
-    var new_datetimeInit = new_datetime.replace(/-/g, '/');
-
-    // 处理成为时间戳
-    timestamp = new Date(Date.parse(new_datetimeInit));
-    timestamp = timestamp.getTime();
-    timestamp = timestamp/1000;
-
-    // 增加8个小时，北京时间比utc时间多八个时区
-    var timestamp = timestamp+8*60*60;
-
-    // 时间戳转为时间
-    //var beijing_datetime = new Date(parseInt(timestamp) * 1000).toLocaleString().replace(/年|月/g, "-").replace(/日/g, " ");
-
-    let dateTemp = new Date(parseInt(timestamp) * 1000);
-    let year = dateTemp.getFullYear();
-    let month = dateTemp.getMonth() + 1;
-    let day = dateTemp.getDate();
-    let hour = dateTemp.getHours();
-    let minute = dateTemp.getMinutes();
-    let dayTemp = year + "/" + month + "/" + day + "  " + hour + ":" + minute;
-    return dayTemp;
-
-
-    //return beijing_datetime; // 2017-03-31 16:02:06
   },
 
   checkFormDtaBeforeSubmit: function(){
